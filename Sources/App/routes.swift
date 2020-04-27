80073058223c16ec032dd8d8c170db0563a45822
Xander Schoeman <Xschoeman@jhb.dvt.co.za>
2020-04-27 14:36:58 +0200

    Added new user post request and get for a single user, also saves to database.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,7 +3,7 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     
-        router.get("users") { request in
+        router.get("userList") { request in
             return """
     [
       {
@@ -33,6 +33,51 @@ public func routes(_ router: Router) throws {
     ]
     """
         }
+//
+    router.get("users") { req -> Future<[User]> in
+        return User.query(on: req).all()
+    }
+    
+    router.get { req -> Future<View> in
+        return User.query(on: req).all().flatMap(to: View.self) { users in
+            let context = ["users": users]
+            return try req.view().render("home", context)
+        }
+    }
+    
+    router.post("send") { req -> Future<User> in
+        let username: String = try req.content.syncGet(at: "username")
+        let password: String = try req.content.syncGet(at: "password")
+        let profileImage: String = try req.content.syncGet(at: "profileImage")
+        let fullName: String = try req.content.syncGet(at: "fullName")
+        let email: String = try req.content.syncGet(at: "email")
+        let gender: String = try req.content.syncGet(at: "gender")
+        let age: Int = try req.content.syncGet(at: "age")
+        let favouriteCount: Int = try req.content.syncGet(at: "favouriteCount")
+        let genrePref: String = try req.content.syncGet(at: "genrePref")
+        //let msg = Message(id: nil, username: username, password: password)
+        let userObj = User(id: nil, username: username, password: password, profileImage: profileImage, fullName: fullName, email: email, gender: gender, age: age, favouriteCount: favouriteCount, genrePref: genrePref)
+        return userObj.save(on: req)
+    }
+    
+//        router.post("authenticate") { req -> Future<User> in
+//        let username: String = try req.content.syncGet(at: "username")
+//        let password: String = try req.content.syncGet(at: "password")
+//            
+//         let someUser = User.query(on: req).all()
+//                
+//                for item in someUser {
+//                    if (item.username == username && item.password == password) {
+//                        return item
+//                    }
+//                }
+//            }
+//        return User()
+//    }
+    
+    
+    
+    
 // Still trying to get the database working correctly
 //------------------------------------------------------------------
 //    router.get { req -> Future<View> in
